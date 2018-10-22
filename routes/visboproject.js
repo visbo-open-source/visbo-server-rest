2d0af8f9de7b1de2e4ca8180b421ed13e453b086
Markus <markus@seyfried.bayern>
2018-10-22 16:22:15 +0200

    Improve e-Mail
    Add Header & Footer to e-Mail Templates to show unform Header & Logo & Footer Information
    Add Support for DKIM (Adopt .env and /etc/visbo/)

--- routes/visboproject.js
+++ routes/visboproject.js
@@ -158,7 +158,7 @@ router.route('/')
 		logger4js.debug("Get Project for user %s check sysAdmin %s status %s", userId, req.query.sysadmin, !req.decoded.status || req.decoded.status.sysAdminRole);
 		if (!req.query.sysadmin || !req.decoded.status || !req.decoded.status.sysAdminRole) {
 			// either member of the project or if project is public member of the VC
-			logger4js.debug("Get Project Public VC %O", req.listVC);
+			logger4js.debug("Get Project Public VC %s", JSON.stringify(req.listVC));
 			query = { $or: [ {'users.email': useremail}, { vpPublic: true, vcid: {$in: req.listVC } } ] }		// Permission for User
 		}
 		query.deleted = {$exists: false};				// Not deleted
