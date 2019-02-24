25574e6fdbde738f1900a22e7c61bd6a662ff38f
Markus <markus@seyfried.bayern>
2019-02-24 08:39:56 +0100

    Update visboprojectversion.js

--- routes/visboprojectversion.js
+++ routes/visboprojectversion.js
@@ -135,7 +135,7 @@ router.route('/')
 		var longList = false;		// show only specific columns instead of all
 		var nowDate = new Date();
 
-		// queryvpv.deletedAt = {$exists: checkDeleted};
+		queryvpv.deletedAt = {$exists: checkDeleted};
 		// collect the VPIDs where the user has View permission to
 		var vpidList = [];
 		if (req.query.vpid) {
@@ -148,7 +148,7 @@ router.route('/')
 						message: 'No Permission to see deleted Versions'
 					});
 				} else {
-					// queryvpv.deletedAt = {$exists: true};
+					queryvpv.deletedAt = {$exists: true};
 				}
 			}
 		} else {
