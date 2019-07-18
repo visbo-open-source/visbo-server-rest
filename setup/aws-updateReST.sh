3d365a7e7efc79daf2b323c49b26e308c0e96937
Markus <markus@seyfried.bayern>
2019-07-18 14:22:06 +0200

    Update Procedures
    - optimised to filter Mongo Std Messages about connection
    - aws-updateReST script for upgrade automation

---
+++ setup/aws-updateReST.sh
@@ -0,0 +1,8 @@
+echo `date` "Execute Update of Visbo ReST by user" `whoami`
+echo `date` "Update ReST Server"
+cd $HOME/GitHub/visbo-server-rest
+bash update-rest
+echo `date` "Update ReST Server UI"
+cd $HOME/GitHub/visbo-server-ui
+bash update-ui
+echo `date` "Update ReST Server done"
