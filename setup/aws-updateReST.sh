04a6e65db92cdce44baeefee55a02eb5408c3709
Markus <markus@seyfried.bayern>
2019-08-22 14:19:49 +0200

    Generate Log File for Updating ReST Server

--- setup/aws-updateReST.sh
+++ setup/aws-updateReST.sh
@@ -1,8 +1,10 @@
-echo `date` "Execute Update of Visbo ReST by user" `whoami`
-echo `date` "Update ReST Server"
-cd $HOME/GitHub/visbo-server-rest
-bash update-rest force
-echo `date` "Update ReST Server UI"
-cd $HOME/GitHub/visbo-server-ui
-bash update-ui
-echo `date` "Update ReST Server done"
+LogFile="/var/log/visbo/$HOSTNAME/"`date '+%Y-%m-%d_%H:%M:%S'`"_UpdateLog"
+echo $LogFile
+echo `date` "Execute Update of Visbo ReST by user" `whoami` | tee -a $LogFile
+echo `date` "Update ReST Server" | tee -a $LogFile
+cd $HOME/GitHub/visbo-server-rest | tee -a $LogFile
+bash update-rest force | tee -a $LogFile
+echo `date` "Update ReST Server UI" | tee -a $LogFile
+cd $HOME/GitHub/visbo-server-ui | tee -a $LogFile
+bash update-ui | tee -a $LogFile
+echo `date` "Update ReST Server done" | tee -a $LogFile
