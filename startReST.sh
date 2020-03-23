9bfc72268eabdefc259bf662d31c43875d474e47
Markus <markus@seyfried.bayern>
2020-03-23 17:44:18 +0100

    api/status deliver date versions in ISO Format

--- startReST.sh
+++ startReST.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd $HOME/GitHub/visbo-ui-build
-export VERSION_UI=`git show -s --pretty=format:"%ci" | head -1`
+export VERSION_UI=`git show -s --pretty=format:"%cI" | head -1`
 cd $HOME/GitHub/visbo-server-rest
-export VERSION_REST=`git show -s --pretty=format:"%ci" | head -1`
+export VERSION_REST=`git show -s --pretty=format:"%cI" | head -1`
 npm start
