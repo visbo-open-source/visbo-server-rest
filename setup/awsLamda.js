a9d086626af2f03648fa012703f0fff9169955d1
Markus <markus@seyfried.bayern>
2020-08-10 14:48:56 +0200

    Update awsLamda.js

--- setup/awsLamda.js
+++ setup/awsLamda.js
@@ -3,10 +3,10 @@ var util = require('util');
 
 exports.handler = function(event, context) {
     console.log('event:'+JSON.stringify(event, null, 2));
-    console.log('From SNS:', event.Records[0].Sns.Message, "len", event.Records[0].Sns.Message.length);
+    console.log('From SNS:', event.Records[0].Sns.Message, 'len', event.Records[0].Sns.Message.length);
 
-    var channel = "#aws_other";
-    var username = "AWS Monitoring";
+    var channel = '#aws_other';
+    var username = 'AWS Monitoring';
     var path = '/services/T753BM10B/BLBNDTCN4/ZUpVYbYDyM0vQHIXUHUwgwoZ';
     var subject = 'Not Set';
     var message = 'Unkown';
@@ -17,60 +17,61 @@ exports.handler = function(event, context) {
         // Handle Notifications with Area set in Subject (i.e. LoadBalancer Activity)
         subject = event.Records[0].Sns.Subject;
         console.log('Subject Available Check Area:', subject);
-        if (subject.indexOf("VisboDev") >= 0) {
-            channel = "#aws_dev";
-        } else if (subject.indexOf("VisboStag") >= 0) {
-            channel = "#aws_stag";
-        } else if (subject.indexOf("VisboProd") >= 0) {
-            channel = "aws_prod";
+        if (subject.indexOf('VisboDev') >= 0) {
+            channel = '#aws_dev';
+        } else if (subject.indexOf('VisboStag') >= 0) {
+            channel = '#aws_stag';
+        } else if (subject.indexOf('VisboProd') >= 0) {
+            channel = 'aws_prod';
         }
         jsonObj = JSON.parse(event.Records[0].Sns.Message);
     } else if (event && event.Records && event.Records.length >= 1 && event.Records[0].Sns && event.Records[0].Sns.Message) {
         // Handle Notifications with Area set in Message (i.e. System manager Upgrade)
-        subject = (JSON.stringify(event.Records[0].Sns.Message)).substr(0,40);
-        console.log('Subject not Available Check Message:', subject);
+        subject = (JSON.stringify(event.Records[0].Sns.Message));
+        console.log('Subject not Available Check Message:', event.Records[0].Sns.Message);
         jsonObj = JSON.parse(event.Records[0].Sns.Message);
-        if (jsonObj["detail-type"] && jsonObj["detail"]) {
-            subject = jsonObj["detail-type"].concat(" Status: ", jsonObj.detail.status || 'Unkown');
+        if (jsonObj['detail-type'] && jsonObj['detail']) {
+            subject = jsonObj['detail-type'].concat(' Status: ', jsonObj.detail.status || 'Unkown');
         }
-        if (event.Records[0].Sns.Message.indexOf("VisboDev") >= 0) {
-            channel = "#aws_dev";
-        } else if (event.Records[0].Sns.Message.indexOf("VisboStag") >= 0) {
-            channel = "#aws_stag";
-        } else if (event.Records[0].Sns.Message.indexOf("VisboProd") >= 0) {
-            channel = "aws_prod";
+        var tmpMessage = event.Records[0].Sns.Message.toLowerCase();
+        if (tmpMessage.indexOf('visbodev') >= 0) {
+            channel = '#aws_dev';
+        } else if (tmpMessage.indexOf('visbostag') >= 0) {
+            channel = '#aws_stag';
+        } else if (tmpMessage.indexOf('visboprod') >= 0) {
+            channel = 'aws_prod';
         } else {
             sendMessage = false;
         }
     }
 
     var postData = {
-        "channel": channel,
-        "username": username,
-        "text": "*" + subject + "*"
+        'channel': channel,
+        'username': username,
+        'text': '*' + subject + '*'
     };
 
-    var severity = "good";
+    var severity = 'good';
 
     // console.log('NewStateValue: ', jsonObj.NewStateValue);
-    if (jsonObj.NewStateValue == "ALARM") {
+    if (jsonObj.NewStateValue == 'ALARM') {
         // console.log('NewStateValue: severity danger');
-        severity = "danger";
-    } else if (jsonObj.NewStateValue == "OK") {
+        severity = 'danger';
+    } else if (jsonObj.NewStateValue == 'OK') {
         // console.log('NewStateValue: severity good');
-        severity = "good";
-    } else if (jsonObj.detail && jsonObj.detail.status == "Failed") {
+        severity = 'good';
+    } else if (jsonObj.detail && jsonObj.detail.status == 'Failed') {
         console.log('Command Execution Failed: severity danger');
-        severity = "danger";
-    } else if (jsonObj.detail && jsonObj.detail.status == "InProgress") {
+        severity = 'danger';
+    } else if (jsonObj.detail && jsonObj.detail.status == 'InProgress') {
         console.log('Command Execution InProgress: severity danger');
-        severity = "warning";
+        severity = 'warning';
     }
     // console.log('jsonObj: ', JSON.stringify(jsonObj));
 
     console.log('Message Severity: ', severity);
-    if (!sendMessage && severity == "good") {
-        console.log("Do not send a Notification in case of Success but without specific info i.e. Check for Updates");
+    if (!sendMessage && severity == 'good') {
+        console.log('Do not send a Notification in case of Success but without specific info i.e. Check for Updates');
         // req.end();
     }
 
@@ -87,7 +88,7 @@ exports.handler = function(event, context) {
         var param = JSON.parse(jsonObj.detail.parameters);
         if (param.commands) {
             var cmd = JSON.stringify(param.commands);
-            if (cmd.indexOf("aws-updateReST.sh") >= 0) {
+            if (cmd.indexOf('aws-updateReST.sh') >= 0) {
                 text = text.concat('Command: \tUpdate Visbo Software\n');
                 extendedMessage = false
             } else {
@@ -113,24 +114,24 @@ exports.handler = function(event, context) {
         for (var comp in jsonObj) {
             var type = typeof jsonObj[comp];
             console.log('Convert Item: ', comp, ' of type ', type);
-            if (type == "string" || type == "number" || type == "boolean") {
-                text = text.concat(comp, ":\t", jsonObj[comp], "\n");
-            } else if (type == "object") {
-                text = text.concat(comp, ":\t", "\n");
+            if (type == 'string' || type == 'number' || type == 'boolean') {
+                text = text.concat(comp, ':\t', jsonObj[comp], '\n');
+            } else if (type == 'object') {
+                text = text.concat(comp, ':\t', '\n');
                 for (var subcomp in jsonObj[comp]) {
                     type = typeof subcomp;
-                    if (type == "string" || type == "number" || type == "boolean") {
-                        text = text.concat('\t', subcomp, ":\t", jsonObj[comp][subcomp], "\n");
+                    if (type == 'string' || type == 'number' || type == 'boolean') {
+                        text = text.concat('\t', subcomp, ':\t', jsonObj[comp][subcomp], '\n');
                     }
                 }
             }
         }
     }
-    console.log("text: ", text);
+    console.log('text: ', text);
     postData.attachments = [
         {
-            "color": severity,
-            "text": text
+            'color': severity,
+            'text': text
         }
     ];
 
@@ -152,6 +153,6 @@ exports.handler = function(event, context) {
       console.log('problem with request: ' + e.message);
     });
 
-    req.write(util.format("%j", postData));
+    req.write(util.format('%j', postData));
     req.end();
 };
