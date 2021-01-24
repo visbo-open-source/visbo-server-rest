a5296746b048e63d9bc6f94de27794d107e62fa3
Markus Seyfried <markus.seyfried@visbo.de>
2021-01-24 12:45:26 +0100

    Create Project from Template extendd Version of vpv copy

---
+++ components/helperVpv.js
@@ -0,0 +1,125 @@
+var mongoose = require('mongoose');
+// var Const = require('../models/constants');
+
+var VisboProject = mongoose.model('VisboProject');
+var VisboProjectVersion = mongoose.model('VisboProjectVersion');
+// var VisboPortfolio = mongoose.model('VisboPortfolio');
+// var VCSetting = mongoose.model('VCSetting');
+
+var validate = require('./../components/validate');
+var errorHandler = require('./../components/errorhandler').handler;
+
+var logModule = 'VPV';
+var log4js = require('log4js');
+var logger4js = log4js.getLogger(logModule);
+
+// updates the VPV Count in the VP after create/delete/undelete VISBO Project
+var updateVPVCount = function(vpid, variantName, increment){
+	var updateQuery = {_id: vpid};
+	var updateOption = {upsert: false};
+	var updateUpdate;
+
+	if (!variantName) {
+		updateUpdate = {$inc: {vpvCount: increment}};
+	} else {
+		// update a variant and increment the version counter
+		updateQuery['variant.variantName'] = variantName;
+		updateUpdate = {$inc : {'variant.$.vpvCount' : increment} };
+	}
+	logger4js.debug('Update VP %s with vpvCount inc %d update: %O with %O', vpid, increment, updateQuery, updateUpdate);
+	VisboProject.updateOne(updateQuery, updateUpdate, updateOption, function (err, result) {
+		if (err){
+			logger4js.error('Problem updating VP %s vpvCount: %s', vpid, err.message);
+		}
+		logger4js.trace('Updated VP %s vpvCount inc %d changed %d %d', vpid, increment, result.n, result.nModified);
+	});
+};
+
+function initVPV(vpv) {
+	var newVPV = new VisboProjectVersion();
+	if (!vpv
+		|| !validate.validateName(vpv.variantDescription, true)
+		|| !validate.validateName(vpv.leadPerson, true)
+		|| !validate.validateName(vpv.ampelErlaeuterung, true)
+		|| !validate.validateName(vpv.VorlagenName, true)
+		|| !validate.validateName(vpv.description, true)
+		|| !validate.validateName(vpv.timestamp, true)
+		|| !validate.validateDate(vpv.startDate, true)
+		|| !validate.validateDate(vpv.endDate, true)
+		|| !validate.validateDate(vpv.earliestStartDate, true)
+		|| !validate.validateDate(vpv.latestStartDate, true)
+	) {
+		logger4js.info('InitVPV bad content', JSON.stringify(vpv));
+		return undefined;
+	}
+
+	// keep unchangable attributes
+	newVPV.name = vpv.name;
+	newVPV.vpid = vpv.vpid;
+	if (vpv.variantName) {
+		newVPV.variantName = vpv.variantName;
+	} else {
+		newVPV.variantName = '';
+	}
+	if (vpv.timestamp && Date.parse(vpv.timestamp)) {
+		newVPV.timestamp = new Date(vpv.timestamp);
+	} else {
+		newVPV.timestamp = new Date();
+	}
+	newVPV.variantDescription = vpv.variantDescription;
+	newVPV.Risiko = vpv.Risiko;
+	newVPV.StrategicFit = vpv.StrategicFit;
+	newVPV.customDblFields = vpv.customDblFields;
+	newVPV.customStringFields = vpv.customStringFields;
+	newVPV.customBoolFields = vpv.customBoolFields;
+	newVPV.actualDataUntil = vpv.actualDataUntil;
+	newVPV.Erloes = vpv.Erloes;
+	newVPV.leadPerson = vpv.leadPerson;
+	newVPV.startDate = vpv.startDate;
+	newVPV.endDate = vpv.endDate;
+	newVPV.earliestStart = vpv.earliestStart;
+	newVPV.earliestStartDate = vpv.earliestStartDate;
+	newVPV.latestStart = vpv.latestStart;
+	newVPV.latestStartDate = vpv.latestStartDate;
+	newVPV.status = vpv.status;
+	newVPV.ampelStatus = vpv.ampelStatus;
+	newVPV.ampelErlaeuterung = vpv.ampelErlaeuterung;
+	newVPV.farbe = vpv.farbe;
+	newVPV.Schrift = vpv.Schrift;
+	newVPV.Schriftfarbe = vpv.Schriftfarbe;
+	newVPV.VorlagenName = vpv.VorlagenName;
+	newVPV.Dauer = vpv.Dauer;
+	newVPV.AllPhases = vpv.AllPhases;
+	newVPV.hierarchy = vpv.hierarchy;
+	newVPV.volumen = vpv.volumen;
+	newVPV.complexity = vpv.complexity;
+	newVPV.description = vpv.description;
+	newVPV.businessUnit = vpv.businessUnit;
+
+	return newVPV;
+}
+
+function saveVPV(req, res, newVPV) {
+	logger4js.warn('Store VPV for vpid %s/%s ', newVPV._id, newVPV.name);
+	delete newVPV._id;
+	newVPV.save(function(err, oneVPV) {
+		if (err) {
+			errorHandler(err, res, 'DB: Create Template VPV Save', 'Error creating Project Version ');
+			return;
+		}
+		req.oneVPV = oneVPV;
+		// update the version count of the base version or the variant
+		updateVPVCount(req.oneVPV.vpid, req.oneVPV.variantName, 1);
+		return res.status(200).send({
+			state: 'success',
+			message: 'Successfully created new Project',
+			vp: [ req.oneVP ]
+		});
+	});
+}
+
+module.exports = {
+	updateVPVCount: updateVPVCount,
+	saveVPV: saveVPV,
+	initVPV: initVPV
+};
