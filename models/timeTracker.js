25904c4fad4e0d2e7c1b4632dfa777ad5e31aaf4
maksym-cheremnov <maksym.cheremnov@wtt-solutions.com>
2023-04-18 19:54:26 +0300

    Add time tracker model and edit .gitignore

---
+++ models/timeTracker.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var timeTracker = new mongoose.Schema({
+    userId: { type: Schema.Types.ObjectId, ref: 'User', required: false },
+    vpid: { type: Schema.Types.ObjectId, ref: 'VisboProject', required: true },
+    vcid: { type: Schema.Types.ObjectId, ref: 'VisboCenter', required: true },
+    roleId: { type: Schema.Types.ObjectId, ref: 'Role', required: true },
+    date: { type: Schema.Types.Date, required: true },
+    time: { type: Schema.Types.Decimal128, required: true },
+    notes: { type: Schema.Types.String, required: false },
+    approvalDate: { type: Schema.Types.Date, required: false },
+    approvalId: { type: Schema.Types.ObjectId, ref: 'User', required: false },
+    status: { type: Schema.Types.String, required: true },
+    aggreUID: { type: Schema.Types.String, required: false },
+    aggreDate: { type: Schema.Types.Date, required: false },
+    vpvid: { type: Schema.Types.ObjectId, ref: 'VisboProjectVersion', required: true },
+    timestamp: { type: Date, required: false },
+});
+// Set Creation and modification date automatically
+timeTracker.set('timestamps', true);
+
+// declare a model
+mongoose.model('TimeTracker', timeTracker);
