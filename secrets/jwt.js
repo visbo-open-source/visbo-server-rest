e402b97d75b9603c755b0dcdf50a9eadffc13fc2
maksym-cheremnov <maksym.cheremnov@wtt-solutions.com>
2023-04-28 14:04:21 +0300

    Hide secretes and update env-empty file

--- secrets/jwt.js
+++ secrets/jwt.js
@@ -1,16 +1,18 @@
+require('dotenv').config();
+
 module.exports = {
 	user: {
-		'secret': 'now you see visual board',
+		'secret': process.env.USER_SECRET,
 		'expiresIn': 3600
 	},
 	admin: {
-		'secret': 'a good day to use visual board',
+		'secret': process.env.ADMIN_SECRET,
 		'expiresIn': 3600
 	},
 	register: {
-		'secret': 'now you start with visual board',
+		'secret': process.env.REGISTER_SECRET,
 		'expiresIn': 900
 	},
 	internalEncryption:
-		{ 'secret': 'M}*/n5jg3P1KLro0)_1-81?/ORBG;W0.' }
+		{ 'secret': process.env.INTERNAL_ENCRYPTION }
 };
