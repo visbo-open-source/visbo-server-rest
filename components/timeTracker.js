6c8d680715396dd4fd58f21a3f74774027dedf3f
maksym-cheremnov <maksym.cheremnov@wtt-solutions.com>
2023-05-08 17:24:04 +0300

    Add array updated

--- components/timeTracker.js
+++ components/timeTracker.js
@@ -1,27 +1,28 @@
 var mongoose = require('mongoose');
 
-var logModule = 'OTHER';
-var log4js = require('log4js');
-var logger4js = log4js.getLogger(logModule);
-
 var TimeTracker = mongoose.model('TimeTracker');
 
 async function createTimeEntry(userId, transaction) {
-    var timeTracker = new TimeTracker({ userId: userId, status: 'Not Started' , ...transaction });
+    var timeTracker = new TimeTracker({ userId: userId, status: 'New', ...transaction });
     await timeTracker.save();
     return timeTracker;
 }
 
 async function updateTimeEntry(id, transaction) {
-    const entry = await TimeTracker.findById(id);
-    if (entry.status && entry.status === 'Approved') {
-        logger4js.error('Cannot update approved time record with id: %s', id);
-        return;
+    if (transaction.approvalList && transaction.approvalList.lenght > 0) {
+        const array = [];
+        transaction.approvalList.forEach(async (id) => {
+            await TimeTracker.updateOne({ _id: id }, { approvalDate: transaction.approvalDate, approvalId: transaction.approvalId, status: transaction.status });
+            var updatedEntry = await TimeTracker.findById(id);
+            array.push(updatedEntry);
+        });
+        return array;
     } else {
         await TimeTracker.updateOne({ _id: id }, transaction);
         var updatedEntry = await TimeTracker.findById(id);
         return updatedEntry;
     }
+
 }
 
 async function deleteTimeEntry(id) {
@@ -30,8 +31,8 @@ async function deleteTimeEntry(id) {
 }
 
 async function getTimeEntry(id) {
-        var timeEntry = await TimeTracker.find({userId: id});
-        return timeEntry;
+    var timeEntry = await TimeTracker.find({ userId: id });
+    return timeEntry;
 }
 
 module.exports = {
